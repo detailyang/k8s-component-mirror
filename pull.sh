cb865b75f561036b3c14b7e5dbf9f0c83f240ff4
detailyang <detailyang@gmail.com>
2018-12-10 20:40:40 +0800

    release: upgrade to kubeadm to 1.13.0

--- pull.sh
+++ pull.sh
@@ -2,13 +2,13 @@
 
 images=(
  "kubernetes-dashboard-amd64:v1.10.0"
- "kube-apiserver:v1.12.2"
- "kube-controller-manager:v1.12.2"
- "kube-scheduler:v1.12.2"
- "kube-proxy:v1.12.2"
+ "kube-apiserver:v1.13.0"
+ "kube-controller-manager:v1.13.0"
+ "kube-scheduler:v1.13.0"
+ "kube-proxy:v1.13.0"
  "pause:3.1"
  "etcd:3.2.24"
- "coredns:1.2.2"
+ "coredns:1.2.6"
  "kube-addon-manager:v8.6"
 )
 
